cab2e43f90f59ec50835e808c16fde01b82b984b
Unai Elizondo <unai.elizondo@biko2.com>
2022-12-23 13:06:44 +0100

    new version

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const createConfigFeatureFlags = require("./createConfigFeatureFlags");
 const featureFlagService = require("./featureFlagService");
 
 module.exports = {
-  createConfigFeatureFlags,
   featureFlagService,
 };
